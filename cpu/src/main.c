2c37c1c8f47c60bcf8a4618cfcdfc53b7dc1e784
utnso <cdiriehaddad@frba.utn.edu.ar>
2026-04-18 03:43:30 +0000

    Todas las conexiones establecidas

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,47 +1,78 @@
-#include "main.h"
 #include <commons/config.h>
 #include <commons/log.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <utils/hello.h>
 #include <utils/utils.h>
 
 int main(int argc, char *argv[]) {
   saludar("cpu");
 
-  // Levanto el logger
-  t_log *logger_Cpu;
-  t_config *config_Cpu;
-
-  logger_Cpu = iniciar_logger();
-
-  log_info(logger_Cpu, "A VER SI ANDA");
-  // Paso archivo de config
-  config_Cpu = iniciar_config();
-
-  log_destroy(logger_Cpu);
-  config_destroy(config_Cpu);
-
-  return 0;
-}
-
-// Funciones
-
-t_log *iniciar_logger(void) {
-  t_log *logger_Cpu;
-  // creo el nuevo logger
-  logger_Cpu = log_create("CPU.log", "CPU", true, LOG_LEVEL_INFO);
-  return logger_Cpu;
-}
-
-t_config *iniciar_config(void) {
-  t_config *config_Cpu;
-
-  // le digo de donde levantar la config
-  config_Cpu = config_create("CPU.config");
-  // oh la la, estoy manejando errores
-  if (config_Cpu == NULL) {
-    perror("Hubo un problema con el config");
-    abort();
+  // valido cantidad de argumentos
+  if (argc < 3) {
+    printf(
+        "Error: Faltan argumentos. Uso: ./bin/cpu [Config] [Identificador]\n");
+    return 1;
   }
 
-  return config_Cpu;
+  char *id_cpu = argv[2];
+
+  // creo el logger
+  char nombre_logger[20];
+  sprintf(nombre_logger, "CPU_%s", id_cpu);
+  t_log *logger = log_create("cpu.log", nombre_logger, true, LOG_LEVEL_INFO);
+
+  if (logger == NULL) {
+    printf("Error al crear el logger\n");
+    return 1;
+  }
+
+  t_config *config = config_create("cpu.config");
+  if (config == NULL) {
+    log_error(logger, "No se pudo encontrar el arhcivo cpu.config");
+    return 1;
+  }
+
+  char *ip_memory = config_get_string_value(config, "IP_MEMORY");
+  char *puerto_memory = config_get_string_value(config, "PUERTO_MEMORY");
+  char *ip_scheduler = config_get_string_value(config, "IP_SCHEDULER");
+  char *puerto_scheduler = config_get_string_value(config, "PUERTO_SCHEDULER");
+  char *ip_memory_stick = config_get_string_value(config, "IP_STICK");
+  char *puerto_memory_stick = config_get_string_value(config, "PUERTO_STICK");
+
+  //Conexion a Kernel Memory
+
+  int conexion_memory = crear_conexion(ip_memory, puerto_memory);
+  if (conexion_memory != -1) {
+    log_info(logger, "## Conectado a Kernel Memory");
+  } else {
+    log_error(logger, "Error al conectar a Kernel Memory");
+  }
+
+  //Conexion a Planificado Kernel
+
+  int conexion_scheduler = crear_conexion(ip_scheduler, puerto_scheduler);
+  if (conexion_scheduler != -1) {
+    log_info(logger, "## Conectado a Kernel Scheduler");
+  } else {
+    log_error(logger, "Error al conectar a Kernel Scheduler");
+  }
+
+  // Conexion a Memory Stick
+  int conexion_stick = crear_conexion(ip_memory_stick, puerto_memory_stick);
+  if (conexion_stick != -1) {
+    log_info(logger, "## Conectado a Memory Stick");
+  } else {
+    log_error(logger, "Error al conectar a Memory Stick");
+  }
+
+  //libero conexiones
+  liberar_conexion(conexion_memory);
+  liberar_conexion(conexion_scheduler);
+  liberar_conexion(conexion_stick);
+  config_destroy(config);
+  log_destroy(logger);
+
+  return 0;;
 }
