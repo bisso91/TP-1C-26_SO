ee3fe4dc31fe8708c169ace01dd8ede7b5198100
utnso <fedebisso@frba.utn.edu.ar>
2026-05-10 15:42:50 +0000

    Acomode las utils y centralize conexiones

---
+++ cpu/src/cpu_utils.c
@@ -0,0 +1,62 @@
+#include "cpu_utils.h"
+
+void inicializar_registros(t_registros *registros) {
+  registros->PC = 0;
+  registros->AX = 0;
+  registros->BX = 0;
+  registros->CX = 0;
+  registros->DX = 0;
+  registros->EAX = 0;
+  registros->EBX = 0;
+  registros->ECX = 0;
+  registros->EDX = 0;
+  registros->SI = 0;
+  registros->DI = 0;
+}
+
+bool cargar_configuracion(t_config_cpu *config_cpu, t_config *config_raw) {
+
+  // 1. Validamos TODAS las claves necesarias para que no rompa
+  if (!config_has_property(config_raw, "IP_MEMORY") ||
+      !config_has_property(config_raw, "PUERTO_MEMORY") ||
+      !config_has_property(config_raw, "IP_SCHEDULER") ||
+      !config_has_property(config_raw, "PUERTO_SCHEDULER") ||
+      !config_has_property(config_raw, "IP_STICK") ||
+      !config_has_property(config_raw, "PUERTO_STICK") ||
+      !config_has_property(config_raw, "LOG_LEVEL")) {
+    return false;
+  }
+
+  // 2. Extraemos los valores de Memoria y Scheduler
+  config_cpu->ip_memory = config_get_string_value(config_raw, "IP_MEMORY");
+  config_cpu->puerto_memory =
+      config_get_string_value(config_raw, "PUERTO_MEMORY");
+
+  config_cpu->ip_scheduler =
+      config_get_string_value(config_raw, "IP_SCHEDULER");
+  config_cpu->puerto_scheduler =
+      config_get_string_value(config_raw, "PUERTO_SCHEDULER");
+
+  // 3. Extraemos los valores del Memory Stick
+  config_cpu->ip_memory_stick = config_get_string_value(config_raw, "IP_STICK");
+  config_cpu->puerto_memory_stick =
+      config_get_string_value(config_raw, "PUERTO_STICK");
+
+  // 4. Manejo del Log Level
+  char *level_str = config_get_string_value(config_raw, "LOG_LEVEL");
+  config_cpu->log_level = log_level_from_string(level_str);
+
+  log_info(logger, "## Configuracion Cargada correctamente...");
+
+  return true;
+}
+// CONECTAR LOS MODULOS....
+int conectar_a_modulo(char *nombre, char *ip, char *puerto, t_log *logger) {
+  int conexion = crear_conexion(ip, puerto);
+  if (conexion != -1) {
+    log_info(logger, "## Conectado a %s", nombre);
+  } else {
+    log_error(logger, "Error al conectar a %s en %s:%s", nombre, ip, puerto);
+  }
+  return conexion;
+}
--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,3 +1,4 @@
+#include "cpu_utils.h"
 #include <commons/config.h>
 #include <commons/log.h>
 #include <stdbool.h>
@@ -8,71 +9,80 @@
 
 int main(int argc, char *argv[]) {
   saludar("cpu");
-
-  // valido cantidad de argumentos
-  if (argc < 3) {
-    printf(
-        "Error: Faltan argumentos. Uso: ./bin/cpu [Config] [Identificador]\n");
-    return 1;
-  }
-
+  /*
+    // valido cantidad de argumentos
+    if (argc < 3) {
+      printf(
+          "Error: Faltan argumentos. Uso: ./bin/cpu [Config]
+    [Identificador]\n"); return 1;
+    }
+*/
+  // Saco validacion porque me rompe el debug
   char *id_cpu = argv[2];
 
   // creo el logger
   char nombre_logger[20];
   sprintf(nombre_logger, "CPU_%s", id_cpu);
-  t_log *logger = log_create("cpu.log", nombre_logger, true, LOG_LEVEL_INFO);
+  t_log *logger_cpu =
+      log_create("cpu.log", nombre_logger, true, LOG_LEVEL_INFO);
 
-  if (logger == NULL) {
+  // Validacion archivo de log
+  if (logger_cpu == NULL) {
     printf("Error al crear el logger\n");
     return 1;
+  } else {
+    printf("Archivo de log creado correctamente\n");
   }
 
-  t_config *config = config_create("cpu.config");
-  if (config == NULL) {
-    log_error(logger, "No se pudo encontrar el arhcivo cpu.config");
+  t_config *config_plana = config_create("cpu.config");
+  t_config_cpu config_cpu;
+
+  // Validacion de archivo de config
+  if (config_plana == NULL) {
+    log_error(logger_cpu, "No se pudo encontrar el archivo cpu.config");
+    return 1;
+  } else {
+    printf("Archivo de cofiguracion creado correctamente\n");
+  }
+
+  t_registros registros;
+
+  cargar_configuracion(&config_cpu, config_plana);
+
+  if (!cargar_configuracion(&config_cpu, config_plana)) {
+    log_error(logger_cpu, "Error al cargar configuración");
     return 1;
   }
 
-  char *ip_memory = config_get_string_value(config, "IP_MEMORY");
-  char *puerto_memory = config_get_string_value(config, "PUERTO_MEMORY");
-  char *ip_scheduler = config_get_string_value(config, "IP_SCHEDULER");
-  char *puerto_scheduler = config_get_string_value(config, "PUERTO_SCHEDULER");
-  char *ip_memory_stick = config_get_string_value(config, "IP_STICK");
-  char *puerto_memory_stick = config_get_string_value(config, "PUERTO_STICK");
+  // CONEXIONES
+  // Aca fd es File descriptor.
 
-  //Conexion a Kernel Memory
+  int fd_memory = conectar_a_modulo("Kernel Memory", config_cpu.ip_memory,
+                                    config_cpu.puerto_memory, logger_cpu);
+  int fd_scheduler =
+      conectar_a_modulo("Kernel Scheduler", config_cpu.ip_scheduler,
+                        config_cpu.puerto_scheduler, logger_cpu);
+  int fd_stick = conectar_a_modulo("Memory Stick", config_cpu.ip_memory_stick,
+                                   config_cpu.puerto_memory_stick, logger_cpu);
 
-  int conexion_memory = crear_conexion(ip_memory, puerto_memory);
-  if (conexion_memory != -1) {
-    log_info(logger, "## Conectado a Kernel Memory");
-  } else {
-    log_error(logger, "Error al conectar a Kernel Memory");
+  // Validacion
+  if (fd_memory == -1 || fd_scheduler == -1 || fd_stick == -1) {
+    log_error(logger_cpu, "No se pudieron establecer todas las conexiones "
+                          "necesarias. Libero memoria y termino programa.");
+    // ... liberar memoria y salir
+    liberar_conexion(fd_memory);
+    liberar_conexion(fd_scheduler);
+    liberar_conexion(fd_stick);
+    return 1;
   }
 
-  //Conexion a Planificado Kernel
+  // libero conexiones
+  liberar_conexion(fd_memory);
+  liberar_conexion(fd_scheduler);
+  liberar_conexion(fd_stick);
+  config_destroy(config_plana);
+  log_destroy(logger_cpu);
 
-  int conexion_scheduler = crear_conexion(ip_scheduler, puerto_scheduler);
-  if (conexion_scheduler != -1) {
-    log_info(logger, "## Conectado a Kernel Scheduler");
-  } else {
-    log_error(logger, "Error al conectar a Kernel Scheduler");
-  }
-
-  // Conexion a Memory Stick
-  int conexion_stick = crear_conexion(ip_memory_stick, puerto_memory_stick);
-  if (conexion_stick != -1) {
-    log_info(logger, "## Conectado a Memory Stick");
-  } else {
-    log_error(logger, "Error al conectar a Memory Stick");
-  }
-
-  //libero conexiones
-  liberar_conexion(conexion_memory);
-  liberar_conexion(conexion_scheduler);
-  liberar_conexion(conexion_stick);
-  config_destroy(config);
-  log_destroy(logger);
-
-  return 0;;
+  return 0;
+  ;
 }
