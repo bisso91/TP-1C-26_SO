c4f8aacf926886a956605b6ca132e7333f803e78
saniglesias <saniglesias@frba.utn.edu.ar>
2026-05-26 16:02:03 -0300

    Defino los utils del kernel_memory y memory_stick

---
+++ memory_stick/src/stick_utils.c
@@ -0,0 +1,9 @@
+// Aca defino la funcion principal del stick, reservo el bloque de memoria con malloc y pongo a esuchar conexiones del cpu y kernel memory entiendo
+#include "stick_utils.h"
+
+void iniciar_operacion_stick(char* config_path, int tamanio) {
+    t_memory_stick_info stick;
+    stick.tamanio_memoria = tamanio;
+
+    // aca deberia ir lo del logger propio
+}
